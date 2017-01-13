e67a2afcccea80c0748f4c3a8de7d2e6b3272b53
Seongho Baek <seonghobaek@gmail.com>
2017-01-13 09:07:24 +0900

    Add label data

--- setup.sh
+++ setup.sh
@@ -14,19 +14,8 @@ cd face_detector
 
 echo "Building face dectector..wait"
 
-make -j4 > /dev/null
+make  > /dev/null
 
-echo "Done."
-popd > /dev/null
-pushd . > /dev/null
-cd face_identifier
-
-echo "Building face identifier..wait"
-if [ -f CMakeCache.txt ]; then
-	rm CMakeCache.txt > /dev/null
-fi
-cmake ./  > /dev/null
-make -j4 > /dev/null
 echo "Done."
 popd > /dev/null
 
