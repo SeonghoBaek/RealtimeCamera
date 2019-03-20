4ea6e7840ef9b0957f5e6b6e3399bb7b05a4e8fb
SeonghoBaek <seonghobaek@gmail.com>
2019-03-20 18:25:17 +0900

    Update train.sh

--- face_register/train.sh
+++ face_register/train.sh
@@ -11,7 +11,7 @@ find . -name *.jpg_f.jpg -exec rm -f {} \;
 find . -name *.jpg_w.jpg -exec rm -f {} \;
 
 echo Run Sample Augmentation
-python augmentation.py
+/usr/bin/python augmentation.py
 
 echo Align face image..Wait
 rm -rf output
@@ -20,16 +20,16 @@ rm -rf output
 #rm -f output/embedding/cache* > /dev/null
 
 echo Sampling Guest Group
-python guest_sampling.py
+/usr/bin/python guest_sampling.py
 
 #../openface/util/align-dlib.py input/user/ align outerEyesAndNose output/aligned/user --size 96
 ../openface/util/align-dlib.py input/user/ align innerEyesAndBottomLip output/aligned/user --size 96
-../openface/util/align-dlib.py input/iguest/ align innerEyesAndBottomLip output/aligned/iguest --size 96
+#../openface/util/align-dlib.py input/iguest/ align innerEyesAndBottomLip output/aligned/iguest --size 96
 #../openface/util/align-dlib.py input/oguest/ align innerEyesAndBottomLip output/aligned/oguest --size 96
 
 echo Embedding aligned face image..Wait
 ../openface/batch-represent/main.lua -cuda -outDir output/embedding/user -data output/aligned/user
-../openface/batch-represent/main.lua -cuda -outDir output/embedding/iguest -data output/aligned/iguest
+#../openface/batch-represent/main.lua -cuda -outDir output/embedding/iguest -data output/aligned/iguest
 #../openface/batch-represent/main.lua -outDir output/embedding/oguest -data output/aligned/oguest
 
 echo Training SVM..Wait
@@ -39,13 +39,13 @@ echo Training SVM..Wait
 
 ./classifier.py --dlibFacePredictor ignore --cuda train --classifier RadialSvm  output/embedding/user
 mv -f output/embedding/user/classifier.pkl ../svm/
-./classifier.py --dlibFacePredictor ignore --cuda train --classifier DBN --epoch 200 output/embedding/user
-mv -f output/embedding/user/classifier.pkl ../dbn/
+# ./classifier.py --dlibFacePredictor ignore --cuda train --classifier DBN --epoch 200 output/embedding/user
+#mv -f output/embedding/user/classifier.pkl ../dbn/
 
-./classifier.py --dlibFacePredictor ignore --cuda train --classifier RadialSvm  output/embedding/iguest
-mv -f output/embedding/iguest/classifier.pkl ../svm/classifier_iguest.pkl
-./classifier.py --dlibFacePredictor ignore --cuda train --classifier DBN --epoch 200 output/embedding/iguest
-mv -f output/embedding/iguest/classifier.pkl ../dbn/classifier_iguest.pkl
+#./classifier.py --dlibFacePredictor ignore --cuda train --classifier RadialSvm  output/embedding/iguest
+# mv -f output/embedding/iguest/classifier.pkl ../svm/classifier_iguest.pkl
+#./classifier.py --dlibFacePredictor ignore --cuda train --classifier DBN --epoch 200 output/embedding/iguest
+#mv -f output/embedding/iguest/classifier.pkl ../dbn/classifier_iguest.pkl
 
 #./classifier.py --dlibFacePredictor ignore --cuda train --classifier RadialSvm  output/embedding/oguest
 #mv -f output/embedding/oguest/classifier.pkl ../svm/classifier_oguest.pkl
